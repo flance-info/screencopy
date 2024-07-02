fe5694e8b30a219992e6cf06945b3d557ab7cd87
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-07-02 18:22:25 +0500

    fixes

--- server/receive.py
+++ server/receive.py
@@ -2,14 +2,18 @@ import socket
 from PIL import Image
 from io import BytesIO
 from .clipboard import send_to_clipboard
+import threading
+
+stop_server = False
 
 def receive_screenshot():
+    global stop_server
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('0.0.0.0', 12345))  # Bind to all available interfaces
     server_socket.listen(1)
     print("Server is running and waiting for connections...")
 
-    while True:
+    while not stop_server:
         client_socket, client_address = server_socket.accept()
         print(f"Connection from {client_address}")
 
@@ -23,7 +27,7 @@ def receive_screenshot():
         client_socket.close()
 
         image = Image.open(BytesIO(image_data))
-        # image.show()
+       # image.show()
 
         output = BytesIO()
         image.convert('RGB').save(output, format='BMP')
@@ -32,3 +36,17 @@ def receive_screenshot():
 
         send_to_clipboard(win32clipboard.CF_DIB, data)
         print("Screenshot copied to clipboard!")
+
+    server_socket.close()
+
+def stop_server_listener():
+    global stop_server
+    import keyboard
+    keyboard.wait('esc')
+    stop_server = True
+    print("Server stopping...")
+
+def start_server():
+    listener_thread = threading.Thread(target=stop_server_listener)
+    listener_thread.start()
+    receive_screenshot()
